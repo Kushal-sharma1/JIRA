820b3da28e4ba0abbee42398484a1dfc533b1f80
Kushal-sharma1 <sharma.k.kushal@gmail.com>
2022-05-15 23:24:55 +0530

    unique uuid

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+var uid = new ShortUniqueId();
 const addbtn = document.querySelector(".add-btn");
 const modalcont =document.querySelector(".modal-cont");
 let colors =['lightpink','lightgreen','lightblue','black'];
 let modalColor = colors[colors.length-1];
 const mainCont =document.querySelector(".main-cont");
 const textArea =document.querySelector("textarea");
-
+let ticketArr=[];
 let isModal =false;
+//event on add button for creating displaying textarea
 addbtn.addEventListener("click",function(){
   if(!isModal){
   modalcont.style.display="flex";
@@ -41,16 +43,35 @@ allPriorityColor.forEach(function(colorele){
 }
 })
 
-function createTicket(ticketColor,data){
+function createTicket(ticketColor,data,ticketId){
+let id = ticketId || uid();
 let ticket = document.createElement("div");
 ticket.setAttribute("class","ticket-cont");
 ticket.innerHTML=`
 <div class ="ticket-color ${ticketColor}"></div>
-<div class="ticket-id"></div>
+<div class="ticket-id">${id}</div>
 <div class="task-area">${data}</div>
 `
 mainCont.appendChild(ticket);
+
+if(!ticketId){
+  ticketArr.push(
+   { ticketColor,
+    data,
+    ticketId:id
+   }
+  );
+  localStorage.setItem("tickets",JSON.stringify(ticketArr));
+}
 }
 
+if(localStorage.getItem("tickets")){
+  ticketArr=JSON.parse(localStorage.getItem("tickets"));
+  ticketArr.forEach(
+    function (obj){
+      createTicket(obj.ticketColor,obj.data,obj.ticketId);
+    }
+  )
+}
 
 
